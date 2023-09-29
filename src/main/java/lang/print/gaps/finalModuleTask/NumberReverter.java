65773b8504e576544c6ff53d68290c16c05f5701
Endorsmoch <a16003872@alumnos.uady.mx>
2023-09-29 10:52:07 -0600

    Finished

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = (number%10)*100;
+        int second = (((number - (first/100)) / 10 ) % 10) * 10 ;
+        int third = (number - (second/10 + first/100)) / 100  ;
+        System.out.println(first+second+third);
     }
 }
